5c03ff9d5c665f605cd43037eaf0fbc20894ab1f
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-13 20:57:11 +0600

    work

--- practice1/rhombus.js
+++ practice1/rhombus.js
@@ -1,7 +1,20 @@
 const myNewid = document.getElementById('rhombusButton').addEventListener('click', () => {
-    const majorValue = document.getElementById('ellipseMajjor');
-    const major = majorValue.value;
+    const majorvalue = document.getElementById('rhombosD1');
+    const major = majorvalue.value;
     const majorfloat = parseFloat(major);
-    console.log(majorfloat);
+    
+    
+
+    const majorvalue1=document.getElementById('rhombosD2');
+    const major1 = majorvalue1.value;
+    const majorfloat1 = parseFloat(major1);
+    
+
+    const result = 0.5 * majorfloat * majorfloat1;
+
+    const area5 = document.getElementById('rhombusArea');
+    area5.innerHTML = result;
+
+    majorvalue.value ='';
+    majorvalue1.value ='';
 }) 
-console.log("dfs");
